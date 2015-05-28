ad0831d9d10137502e9d10a04f0d16379bb7432f
Nicolas HUMBERT <nicolas.humbert@live.fr>
2015-05-28 09:50:44 -0700

    Update angular-google-plus.js

--- angular-google-plus.js
+++ angular-google-plus.js
@@ -1,6 +1,6 @@
 app.service('Google', ['$http', '$rootScope', '$q', function ($http, $rootScope, $q) {
-            var clientId = '800007886348-jf4lphn17vvm3k5rbtkbu6rl9jcl0954.apps.googleusercontent.com',
-              apiKey = 'AIzaSyABxuvVtIY-upUIia4UEjqoEdTJpno9WvA',
+            var clientId = 'YOUR_CLIENT_ID',
+              apiKey = 'YOUR_API_KEY',
               scopes = [/*'https://www.googleapis.com/auth/plus.login',*/'https://www.googleapis.com/auth/contacts.readonly'],
               deferred = $q.defer();
  
@@ -98,82 +98,3 @@ app.service('Google', ['$http', '$rootScope', '$q', function ($http, $rootScope,
 }]);
 
 
-app.run([function() {
-            var a = document.createElement("script");
-            a.type = "text/javascript", a.async = !0, a.src = "https://js.live.net/v5.0/wl.js";
-            var b = document.getElementsByTagName("script")[0];
-            b.parentNode.insertBefore(a, b)
-}]).service('Live', ['$http', '$rootScope', '$q', function ($http, $rootScope, $q) {
-
-        var scopesArr = ['wl.signin', 'wl.basic', 'wl.emails','wl.contacts_emails'];
-
-        WL.init({
-                client_id: '0000000040157235',
-                redirect_uri: 'http://v2.live2times.com/app',
-                response_type: "token",
-                //logging: true
-            });
-
-        this.signUserIn = function() {
-            var deferred = $q.defer();
-          
-            WL.login({ scope: scopesArr }).then(
-                function (response) {
-                    deferred.resolve(response);
-                },
-                function(responseFailed){
-                    deferred.reject('error');
-                }
-            );
-            return deferred.promise;
-        }
-
-        this.loginStatus = function() {
-            WL.getLoginStatus(function(response) { 
-            alert("response " + response );
-
-            });
-        }
-
-        this.getData = function() {
-            var deferred = $q.defer();
-            WL.api(
-                {
-                    path: "me/contacts",
-                    method: "GET",
-                    scope: scopesArr
-                },
-                    function (result) {
-
-                        if (!result.error) {
-                            var resultData = result.data;
-                            //console.log('resultData',resultData);
-
-                            var emailHashes = new Array;                    
-                            for (i = 0; i < resultData.length; i++) {
-
-                                for (j = 0; j < resultData[i].email_hashes.length; j++) {
-                                    alert( "resultData " + resultData[i].email_hashes[j]);
-                                    emailHashes[emailHashes.length] = resultData[i].email_hashes[j];
-                                }
-                            }
-                            /*var resultText = "";
-                            for (k = 0; k < emailHashes.length; k++) {
-                                resultText += emailHashes[k] + "\r\n";                    
-                            }*/
-
-                           deferred.resolve(emailHashes);
-                        }
-                        else {
-                            alert("Error getting contacts: " + result.error.message);
-                        }
-            });
-
-            return deferred.promise;
-
-        }
-
-        this.signUserOut = function() {
-            WL.logout();
-        }
-    }]);
